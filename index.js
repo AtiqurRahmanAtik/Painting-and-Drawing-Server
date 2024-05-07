6bf6dedc89a671c22d6631fe8ece3b7cef2d8379
AtiqurRahmanAtik <atikahmedtanvir80@gmail.com>
2024-05-07 11:50:10 +0600

    vercel live link problem solve

--- index.js
+++ index.js
@@ -7,7 +7,19 @@ const port = process.env.PORT || 5000;
 
 
 //middleWare 
-app.use(cors())
+// app.use(cors())
+//Must remove "/" from your production URL
+//Must remove "/" from your production URL
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://painting-and-drawing-2f67c.web.app",
+      "https://painting-and-drawing-2f67c.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json())
 
 
@@ -17,7 +29,7 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aq01puw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-console.log(uri);
+// console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -146,7 +158,7 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
